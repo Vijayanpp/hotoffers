099cb68894d63e1dc978c06cda142c97636d7073
Vijayan PP <vijayan.pp@pitsolutions.com>
2017-06-07 15:52:24 +0530

    basic intergration second stage

--- src/app/home/components/latest/latest.component.ts
+++ src/app/home/components/latest/latest.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 @Component({
   selector: 'app-latestdeals',
   templateUrl: './latest.component.html',
   styleUrls: ['./latest.component.css']
 })
 export class LatestComponent implements OnInit {
+  public saleoftheday: FirebaseListObservable<any>;
+  public musthave: FirebaseListObservable<any>;
+  public onsale: FirebaseListObservable<any>;
+  public bestseller: FirebaseListObservable<any>;
 
-  constructor() { }
+  constructor(public db: AngularFireDatabase) {
+    this.saleoftheday = db.list('/Saleoftheday');
+    this.musthave = db.list('/Musthave');
+    this.onsale = db.list('/Onsale');
+    this.bestseller = db.list('/Bestseller');
 
-  ngOnInit() {
+  }
+
+  ngOnInit() {  
   }
 
 }
